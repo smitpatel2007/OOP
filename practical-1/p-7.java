e1d2bfb17e959a4d0436a5aa3cac17756277182d
smit patel <smitpatel1347@gmail.com>
2025-07-30 21:34:10 +0530

    Update p-7.java

--- practical-1/p-7.java
+++ practical-1/p-7.java
@@ -1,1 +1,22 @@
+public class AreaPerimeter {
+    public static void main(String[] args) {
+        // Triangle values
+        double base = 5.0;
+        double height = 10.0;
+
+        // Rectangle values
+        double length = 8.0;
+        double breadth = 4.0;
+
+        // Calculate area of triangle
+        double area = 0.5 * base * height;
+
+        // Calculate perimeter of rectangle
+        double perimeter = 2 * (length + breadth);
+
+        // Display results
+        System.out.println("Area of Triangle = " + area);
+        System.out.println("Perimeter of Rectangle = " + perimeter);
+    }
+}
 
