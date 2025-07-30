07d39e5c6cc24ebd6a1b5002c6aa9d0f694fac44
smit patel <smitpatel1347@gmail.com>
2025-07-30 21:37:46 +0530

    Update p-6.java

--- practical-1/p-6.java
+++ practical-1/p-6.java
@@ -1,1 +1,7 @@
+public class p_6 {
+    public static void main(String[] args) {
+      system.out.println("########################");
+       system.out.println("# 'keep learnig java' #");
+      system.out.println("########################");
+}
 
