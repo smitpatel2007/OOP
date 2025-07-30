863ab2ac613066679353c75a2323dd9a562184f5
smit patel <smitpatel1347@gmail.com>
2025-07-30 21:33:04 +0530

    Update p-8.java

--- practical-1/p-8.java
+++ practical-1/p-8.java
@@ -1,1 +1,12 @@
+public class TempConverter {
+    public static void main(String[] args) {
+        double celsius = 30.0;  // You can change this value
+        double fahrenheit;
+
+        fahrenheit = (celsius * 1.8) + 32;
+
+        System.out.println("Temperature in Celsius: " + celsius);
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+    }
+}
 
