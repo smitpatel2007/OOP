78153066a1e908b8b42e3f38ae2f3e824c9748d9
smit patel <smitpatel1347@gmail.com>
2025-07-31 22:30:47 +0530

    Update p-5.java

--- practical-1/p-5.java
+++ practical-1/p-5.java
@@ -30,3 +30,34 @@ class Main {
         input.close();  // Close scanner
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+pr1_5.java
+    public class pr1_5
+{
+public static void main(String[] args)
+{
+String rollNumber = args[0];
+String name = args[1];
+String course=args[2];
+String semester=args[3];
+
+System.out.println("(a) on a single line:");
+System.out.println("rollNumber:" + rollNumber + ", name:" + name +", course:" + course + ", semester:" + semester);
+
+System.out.println("\n(b) Each on a separate line:");
+System.out.println("roll Number: " + rollNumber);
+System.out.println("name: " + name);
+System.out.println("course: " + course);
+System.out.println("semester: " + semester);
+}
+}
