59f09bcd78d5763780257114cd54bfe9b4fffc88
smit patel <smitpatel1347@gmail.com>
2025-08-05 12:07:48 +0530

    Update p-9.java

--- practical-1/p-9.java
+++ practical-1/p-9.java
@@ -1,1 +1,12 @@
+public class pr1_9
+{
+public static void main(String[] args)
+{
+String name = args[0];
+String age = args[1];
 
+System.out.println("Hello , " + name + "! You are " + age + " years old ");
+System.out.println("name:" + name);
+System.out.println("age:" + age);
+}
+}
