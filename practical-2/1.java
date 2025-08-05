632184079996dfe750a8a69ef04fe0cc7dab85a6
smit patel <smitpatel1347@gmail.com>
2025-08-05 12:48:12 +0530

    Update 1.java

--- practical-2/1.java
+++ practical-2/1.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+
+public class MaxOfThree {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Ask the user to enter 3 numbers
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = input.nextInt();
+
+        // Find maximum number
+        int max = num1;
+
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+
+        // Print the result
+        System.out.println("Maximum number is: " + max);
+    }
+}
 
