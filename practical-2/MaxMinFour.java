c523e402b08374fbc9ececc5bed20a55c5cf2fd8
smit patel <smitpatel1347@gmail.com>
2025-08-05 22:57:17 +0530

    Update MaxMinFour.java

--- practical-2/MaxMinFour.java
+++ practical-2/MaxMinFour.java
@@ -1,59 +1,88 @@
 import java.util.Scanner;
 
-public class MaxMinFour {
+public class MaxMinNestedIfElse {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter four numbers: ");
-        int a = input.nextInt();
+
+        // Input
+        System.out.print("Enter first number: ");
+        int a = input.nextInt(); 
+
+        System.out.print("Enter second number: ");
         int b = input.nextInt();
+
+        System.out.print("Enter third number: ");
         int c = input.nextInt();
+
+        System.out.print("Enter fourth number: ");
         int d = input.nextInt();
 
-        // Max
-        int max;
-        if (a > b)
-            if (a > c)
-                if (a > d)
+        int max, min;
+
+        // Find maximum
+        if (a > b) {
+            if (a > c) {
+                if (a > d) {
                     max = a;
-                else
+                } else {
                     max = d;
-            else if (c > d)
-                max = c;
-            else
-                max = d;
-        else if (b > c)
-            if (b > d)
-                max = b;
-            else
-                max = d;
-        else if (c > d)
-            max = c;
-        else
-            max = d;
+                }
+            } else {
+                if (c > d) {
+                    max = c;
+                } else {
+                    max = d;
+                }
+            }
+        } else {
+            if (b > c) {
+                if (b > d) {
+                    max = b;
+                } else {
+                    max = d;
+                }
+            } else {
+                if (c > d) {
+                    max = c;
+                } else {
+                    max = d;
+                }
+            }
+        }
 
-        // Min
-        int min;
-        if (a < b)
-            if (a < c)
-                if (a < d)
+        // Find minimum
+        if (a < b) {
+            if (a < c) {
+                if (a < d) {
                     min = a;
-                else
+                } else {
                     min = d;
-            else if (c < d)
-                min = c;
-            else
-                min = d;
-        else if (b < c)
-            if (b < d)
-                min = b;
-            else
-                min = d;
-        else if (c < d)
-            min = c;
-        else
-            min = d;
+                }
+            } else {
+                if (c < d) {
+                    min = c;
+                } else {
+                    min = d;
+                }
+            }
+        } else {
+            if (b < c) {
+                if (b < d) {
+                    min = b;
+                } else {
+                    min = d;
+                }
+            } else {
+                if (c < d) {
+                    min = c;
+                } else {
+                    min = d;
+                }
+            }
+        }
 
-        System.out.println("Maximum = " + max);
-        System.out.println("Minimum = " + min);
+        // Output
+        System.out.println("Maximum number = " + max);
+        System.out.println("Minimum number = " + min);
     }
 }
