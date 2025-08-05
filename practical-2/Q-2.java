6561758d94e1a636c6d25938f9e6bdc27ad8bd5d
smit patel <smitpatel1347@gmail.com>
2025-08-05 13:03:12 +0530

    Update Q-2.java

--- practical-2/Q-2.java
+++ practical-2/Q-2.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+
+public class FloatOperations {
+    public static void main(String[] args) {
+        // Create Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter first float number: ");
+        float num1 = input.nextFloat();
+
+        System.out.print("Enter second float number: ");
+        float num2 = input.nextFloat();
+
+        // Perform operations
+        float sum = num1 + num2;
+        float difference = num1 - num2;
+        float product = num1 * num2;
+        float quotient = num1 / num2;
+        float modulus = num1 % num2;
+
+        // Display results
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Modulus = " + modulus);
+
+        // Close the scanner
+        input.close();
+    }
+}
 
