df9a7ff2802d6583852eb824016680658a5fae43
smit patel <smitpatel1347@gmail.com>
2025-08-06 15:02:37 +0530

    Update EvenOdd.java

--- practical-2/EvenOdd.java
+++ practical-2/EvenOdd.java
@@ -13,3 +13,25 @@ public class EvenOdd {
     }
 }
 
+
+
+
+
+// command line
+public class MaxofThree {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a number as a command line argument.");
+            return;
+        }
+
+        
+        int num = Integer.parseInt(args[0]);
+
+        if (num % 2 == 0)
+            System.out.println("Even number");
+        else
+            System.out.println("Odd number");
+    }
+}
+
