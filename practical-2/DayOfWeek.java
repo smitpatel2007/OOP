ade9305a3fadf135d2186b8168dcd0cfe2596ce5
smit patel <smitpatel1347@gmail.com>
2025-08-07 20:53:44 +0530

    Update DayOfWeek.java

--- practical-2/DayOfWeek.java
+++ practical-2/DayOfWeek.java
@@ -19,3 +19,48 @@ public class DayOfWeek {
     }
 }
 
+
+
+
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        // Check if an argument is provided
+        if (args.length == 0) {
+            System.out.println("Please enter a number (1 to 7) as a command line argument.");
+            return;
+        }
+
+        // Convert command line argument to integer
+        int day = Integer.parseInt(args[0]);
+
+        // Switch case to display the day
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid Number!");
+        }
+    }
+}
+
+
