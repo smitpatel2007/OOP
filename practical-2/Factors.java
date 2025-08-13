d1514d20bb0e5f1df4b2c14bb00c249c7a756f3e
smit patel <smitpatel1347@gmail.com>
2025-08-13 13:06:50 +0530

    Update Factors.java

--- practical-2/Factors.java
+++ practical-2/Factors.java
@@ -18,3 +18,29 @@ public class Factors {
         }
     }
 }
+
+
+
+
+// using command line
+
+public class Factors {
+    public static void main(String[] args) {
+        
+        
+        if (args.length != 1) {
+            System.out.println("Please enter one number as a command line argument.");
+            return; 
+        }
+        
+        
+        int num = Integer.parseInt(args[0]);
+        
+        System.out.println("Factors of " + num + " are:");
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) { 
+                System.out.println(i);
+            }
+        }
+    }
+}
