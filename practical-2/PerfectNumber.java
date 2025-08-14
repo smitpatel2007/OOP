736f3f07f57abc81e361ff1a90c04b9c3d262db7
smit patel <smitpatel1347@gmail.com>
2025-08-14 11:56:43 +0530

    Update PerfectNumber.java

--- practical-2/PerfectNumber.java
+++ practical-2/PerfectNumber.java
@@ -19,3 +19,22 @@ public class PerfectNumber {
     }
 }
 
+// command line
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int sum = 0;
+
+   
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0)
+                sum += i;
+        }
+        if (sum == num)
+            System.out.println(num + " is a Perfect Number");
+        else
+            System.out.println(num + " is not a Perfect Number");
+    }
+}
+
+
