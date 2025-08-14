dec52d35b79c9b4a26bed2bd019195b11eb26317
smit patel <smitpatel1347@gmail.com>
2025-08-14 11:54:10 +0530

    Update SumOfDigits.java

--- practical-2/SumOfDigits.java
+++ practical-2/SumOfDigits.java
@@ -16,3 +16,17 @@ public class SumOfDigits {
     }
 }
 
+// command line
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int sum = 0;
+
+        // Calculate sum of digits
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("Sum of digits = " + sum);
+    }
+}
