b87921090c55e7206be3a47ce85ea33f89068d58
smit patel <smitpatel1347@gmail.com>
2025-08-14 12:27:01 +0530

    Create NumberTriangle.java

---
+++ practical-2/NumberTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumberTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
