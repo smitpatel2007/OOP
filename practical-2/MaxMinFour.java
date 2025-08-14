a09ea22054db76956419419c97056eb7b0de5a09
smit patel <smitpatel1347@gmail.com>
2025-08-14 12:11:12 +0530

    Update MaxMinFour.java

--- practical-2/MaxMinFour.java
+++ practical-2/MaxMinFour.java
@@ -86,3 +86,83 @@ public class MaxMinFour {
         System.out.println("Minimum number = " + min);
     }
 }
+
+
+// command line
+public class MaxMinFour {
+    public static void main(String[] args) {
+        // Convert command line arguments to integers
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        int d = Integer.parseInt(args[3]);
+
+        int max, min;
+
+        // Find maximum
+        if (a > b) {
+            if (a > c) {
+                if (a > d) {
+                    max = a;
+                } else {
+                    max = d;
+                }
+            } else {
+                if (c > d) {
+                    max = c;
+                } else {
+                    max = d;
+                }
+            }
+        } else {
+            if (b > c) {
+                if (b > d) {
+                    max = b;
+                } else {
+                    max = d;
+                }
+            } else {
+                if (c > d) {
+                    max = c;
+                } else {
+                    max = d;
+                }
+            }
+        }
+
+        // Find minimum
+        if (a < b) {
+            if (a < c) {
+                if (a < d) {
+                    min = a;
+                } else {
+                    min = d;
+                }
+            } else {
+                if (c < d) {
+                    min = c;
+                } else {
+                    min = d;
+                }
+            }
+        } else {
+            if (b < c) {
+                if (b < d) {
+                    min = b;
+                } else {
+                    min = d;
+                }
+            } else {
+                if (c < d) {
+                    min = c;
+                } else {
+                    min = d;
+                }
+            }
+        }
+
+        // Output
+        System.out.println("Maximum number = " + max);
+        System.out.println("Minimum number = " + min);
+    }
+}
