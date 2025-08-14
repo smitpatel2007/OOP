251d3477297931a26d469b01cb905159529d7e26
smit patel <smitpatel1347@gmail.com>
2025-08-14 11:40:30 +0530

    Update FloatOperations.java

--- practical-2/FloatOperations.java
+++ practical-2/FloatOperations.java
@@ -31,3 +31,27 @@ public class FloatOperations {
     }
 }
 
+// command line
+public class FloatOperations {
+    public static void main(String[] args) {
+        // Convert command line arguments (Strings) to floats
+        float num1 = Float.parseFloat(args[0]);
+        float num2 = Float.parseFloat(args[1]);
+
+        // Perform operations
+        float sum = num1 + num2;
+        float difference = num1 - num2;
+        float product = num1 * num2;
+        float quotient = num1 / num2;
+        float modulus = num1 % num2;
+
+        // Display results
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Modulus = " + modulus);
+    }
+}
+
+
