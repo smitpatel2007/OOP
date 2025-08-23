7c69938449fb36e87a97dbc2746b521cfb0c92eb
smit patel <smitpatel1347@gmail.com>
2025-08-23 22:13:24 +0530

    Create ReverseWords.java

---
+++ practical-3/ReverseWords.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split(" ");
+
+        System.out.println("Words in reverse order:");
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i] + " ");
+        }
+
+        sc.close();
+    }
+}
