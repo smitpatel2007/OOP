44473a94a541be5c661df46dc95c80f0790babd3
smit patel <smitpatel1347@gmail.com>
2025-08-23 22:10:02 +0530

    Create  pr3_6.java

---
+++ practical-3/pr3_6.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class pr3_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int vowels = 0, consonants = 0, digits = 0, special = 0;
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                    vowels++;
+                else
+                    consonants++;
+            } else if (ch >= '0' && ch <= '9') {
+                digits++;
+            } else if (ch != ' ') {
+                special++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("Special Characters: " + special);
+
+        sc.close();
+    }
+}
