c058aa0b8f11725d202b46610b87eb8a4def8dd4
smit patel <smitpatel1347@gmail.com>
2025-09-13 22:34:23 +0530

    Update pr4_1.java

--- practical-4/pr4_1.java
+++ practical-4/pr4_1.java
@@ -1,1 +1,28 @@
+class Student {
+    String name;
+    int id;
+    String course;
+
+    // Method to set details
+    void setDetails(String n, int i, String c) {
+        name = n;
+        id = i;
+        course = c;
+    }
+
+    // Method to display details
+    void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Student ID: " + id);
+        System.out.println("Course: " + course);
+    }
+}
+
+public class pr4_1 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setDetails("Smit", 101, "Computer Science");
+        s1.displayDetails();
+    }
+}
 
