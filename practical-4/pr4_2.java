a12b16b90f40a515fb0f3717dc96d7bc37195923
smit patel <smitpatel1347@gmail.com>
2025-09-13 22:39:03 +0530

    Update pr4_2.java

--- practical-4/pr4_2.java
+++ practical-4/pr4_2.java
@@ -1,1 +1,44 @@
+class BankAccount {
+    int accountNumber;
+    String accountHolderName;
+    double balance;
+
+    // Method to deposit money
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    // Method to withdraw money
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient Balance!");
+        }
+    }
+
+    // Method to display balance
+    void displayBalance() {
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+    }
+}
+
+
+public class pr4_2 {
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+        acc.accountNumber = 12345;
+        acc.accountHolderName = "Smit Patel";
+        acc.balance = 5000;
+
+        acc.displayBalance();
+        acc.deposit(2000);
+        acc.withdraw(1500);
+        acc.displayBalance();
+    }
+}
 
