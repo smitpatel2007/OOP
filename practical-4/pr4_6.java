0a39565e3acd5923567e28a49cb5c8eb9e0d3861
smit patel <smitpatel1347@gmail.com>
2025-09-13 22:48:26 +0530

    Update pr4_6.java

--- practical-4/pr4_6.java
+++ practical-4/pr4_6.java
@@ -1,1 +1,43 @@
+class MaxNumber {
+    // Method 1: Find max of 2 integers
+    int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    // Method 2: Find max of 3 integers
+    int max(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    // Method 3: Find max of 2 double values
+    double max(double a, double b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
+
+public class pr4_6 {  
+    public static void main(String[] args) {
+        // Create object of MaxNumber class
+        MaxNumber m = new MaxNumber();
+
+        // Call from max() method
+        System.out.println("Maximum of 10 and 20 = " + m.max(10, 20));
+        System.out.println("Maximum of 5, 15 and 25 = " + m.max(5, 15, 25));
+        System.out.println("Maximum of 12.5 and 9.8 = " + m.max(12.5, 9.8));
+    }
+}
 
